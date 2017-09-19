0eef8d02a44bdb678ce45a5104c6b97b5aaf9ebf
Takanori Watanabe <takanori5064@gmail.com>
2017-09-19 13:39:03 +0900

    reply.pyを追加

--- oauth_init.py
+++ oauth_init.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import twitter
-# from requests_oauthlib import OAuth1Session
+from requests_oauthlib import OAuth1Session
 from system import conf
 
 # url = "https://api.twitter.com/1.1/statuses/update.json"
@@ -11,4 +11,11 @@ auth = twitter.OAuth(
 	consumer_secret=conf.CONSUMER_SECRET,
 	token=conf.ACCESS_TOKEN_KEY,
 	token_secret=conf.ACCESS_TOKEN_SECRET,
+	)
+
+auth1 = OAuth1Session(
+	conf.CONSUMER_KEY,
+	conf.CONSUMER_SECRET,
+	conf.ACCESS_TOKEN_KEY,
+	conf.ACCESS_TOKEN_SECRET,
 	)
--- reply.py
+++ reply.py
@@ -2,15 +2,34 @@
 
 import twitter
 import oauth_init
+import json
 
-t_userstream = twitter.TwitterStream(auth=auth,domain='userstream.twitter.com')
+t = twitter.Twitter(auth=oauth_init.auth)
+url = "https://api.twitter.com/1.1/statuses/home_timeline.json"
+domain = 'userstream.twitter.com'
 
 def readUserstream():
-	#r = requests.post(url, auth=auth, stream=True, data={"track":"@"})
+	t_userstream = twitter.TwitterStream(auth=oauth_init.auth,domain=domain)
 
-	count = 0
 	for msg in twitter.userstream.user():
 		print(msg)
-		if count > 10:
-			break
-		count+=1
+
+def getTimeline():
+	#r = requests.post(url, auth=auth, stream=True, data={"track":"@"})
+	params = {
+			"count":200, #ツイートを最新から何件取得するか(最大200件)
+			"include_entities" : 1, #エンティティ(画像のURL等)をツイートに含めるか
+			"exclude_replies" : 1, #リプライを含めるか
+			}
+
+	req = oauth_init.auth1.get(url, params=params)
+
+	if req.status_code == 20:
+		timeline = json.loads(req.text)
+	
+		for tweet in timeline:
+			print(tweet["text"])
+	else:
+		print("Error: %d" % req.status_code)
+
+readUserstream()
