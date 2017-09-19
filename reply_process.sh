b57aad1f8d0035fa64f9ff277a2201059be2cab5
Takanori Watanabe <takanori5064@gmail.com>
2017-09-19 18:02:02 +0900

    pathをフルパスに変更

--- reply_process.sh
+++ reply_process.sh
@@ -1,2 +1,2 @@
-sudo ~/.pyenv/versions/3.6.1/bin/python /home/pi/projects/SoraTwitterBot/reply.py &
+~/.pyenv/versions/3.6.1/bin/python /home/pi/projects/SoraTwitterBot/reply.py &
 echo $! > /home/pi/projects/SoraTwitterBot/reply_py.pid
