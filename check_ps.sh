7fd592b627f8a3c631300dad8cc4d0b3425bf27c
Takanori Watanabe <takanori5064@gmail.com>
2017-09-21 04:57:58 +0900

    プロセス監視用シェルスクリプト追加

--- check_ps.sh
+++ check_ps.sh
@@ -2,8 +2,10 @@
 # プロセス監視用シェル
 pid=$(cat /home/pi/projects/SoraTwitterBot/reply_py.pid)
 proc=$(ps)
+echo "this is checker"
 
 if [[ !($proc =~ $pid) ]]; then
  	# プロセス再起動
  	sh /home/pi/projects/SoraTwitterBot/reply_process.sh
+ 	# プロセス再起動をTwitterに通知
 fi
