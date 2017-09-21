db047f910998f2c646bd4b1fba03eed1db4519c9
Takanori Watanabe <takanori5064@gmail.com>
2017-09-21 17:57:45 +0900

    ユーザーストリーム再接続をTwitterに通知できるように変更

--- check_ps.sh
+++ check_ps.sh
@@ -7,5 +7,5 @@ if [[ !($proc =~ $pid) ]]; then
  	# プロセス再起動
  	sh /home/pi/projects/SoraTwitterBot/reply_process.sh
  	# プロセス再起動をTwitterに通知
- 	~/.pyenv/versions/3.6.1/bin/python /home/pi/projects/SoraTwitterBot/Sora.py restart
+ 	~/.pyenv/versions/3.6.1/bin/python /home/pi/projects/SoraTwitterBot/Sora.py restart > /home/pi/projects/SoraTwitterBot/restart.log
 fi
