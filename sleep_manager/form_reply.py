5938fcd7e4936bb1066d9b18bb8993484ff11bd9
Takanori Watanabe <takanori5064@gmail.com>
2017-12-02 19:47:57 +0900

    最初のリプライで日付をツイートするように変更

--- sleep_manager/form_reply.py
+++ sleep_manager/form_reply.py
@@ -4,6 +4,7 @@ import os, sys
 sys.path.append('/home/pi/projects/SoraTwitterBot/')
 import oauth_init
 import event_flags
+import datetime
 import tweepy
 
 api = tweepy.API(oauth_init.auth)
@@ -13,7 +14,12 @@ def reply_introduction():
     event_flags.set_event_flags(event_flags.SLEEP_MANAGER_EVENT)
 
     reply_name = '@sansuke05'
-    status = reply_name + ' マスター、昨日の睡眠時間の記録するよ〜\nまず、就寝時間を教えてね'
+    # 昨日の日付を作成
+    yesterday = datetime.date.today() - datetime.timedelta(days=1)
+    str_yesterday = str(yesterday.month) + '月' + str(yesterday.day) + '日'
+
+    status = reply_name + ' マスター、' + str_yesterday + \
+    'の睡眠時間の記録するよ〜\nまず、就寝時間を教えてね'
 
     print(status)
 
@@ -26,9 +32,15 @@ def reply_sleep_manager_responce(text,user_name):
     responce_counter += 1
     print(responce_counter)
 
+    #if responce_counter == 1:
+    #    time = get_time(text)
+
     #debug
     return 'SUCCESS.'
 
+
+def get_time(text):
+    return text
 
 
 if __name__ == '__main__':
